174e8eec0dab73ddbb8045a9f06b18b3aa707f89
spiral <spiral@spiral.sh>
2020-10-16 17:02:52 -0500

    :x:'nt

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -5,8 +5,7 @@ module.exports = async (msg, bot) => {
     if (!msg.member && process.env.allow_private) 
       msg.member = await bot.getRESTGuildMember(process.env.guild, msg.author.id);
 
-    if (!msg.member || msg.member.guild.id != process.env.guild || !msg.member.roles.includes('722973436074262599'))
-      return bot.addMessageReaction(msg.channel.id, msg.id, "\u274c");
+
 
     let args = msg.content.trim().split(" ");
     if (args.length < 2 || args.shift() != process.env.prefix) 
@@ -14,6 +13,9 @@ module.exports = async (msg, bot) => {
     
     let cmd = bot.commands[args.shift()];
     if (!cmd) return;
+    
+    if (!msg.member || msg.member.guild.id != process.env.guild || !msg.member.roles.includes(process.env.role))
+      return bot.addMessageReaction(msg.channel.id, msg.id, "\u274c");
     
     try { 
         await bot.createMessage(msg.channel.id, await cmd.execute(bot, msg, args)); 
