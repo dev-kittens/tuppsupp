73feb9f07a3bfae7f6845eef9bc41350b8462279
Keterr <kartelant@gmail.com>
2020-10-16 16:48:53 -0600

    fix :x:

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -5,9 +5,6 @@ module.exports = async (msg, bot) => {
     if (!msg.member && process.env.allow_private) 
       msg.member = await bot.getRESTGuildMember(process.env.guild, msg.author.id);
 
-    if (!msg.member || msg.member.guild.id != process.env.guild || !msg.member.roles.includes('722973436074262599'))
-      return bot.addMessageReaction(msg.channel.id, msg.id, "\u274c");
-
     let args = msg.content.trim().split(" ");
     if (args.length < 2 || args.shift() != process.env.prefix) 
       return;
@@ -15,9 +12,13 @@ module.exports = async (msg, bot) => {
     let cmd = bot.commands[args.shift()];
     if (!cmd) return;
     
+    if (!msg.member || msg.member.guild.id != process.env.guild || !msg.member.roles.includes(process.env.role))
+    return bot.addMessageReaction(msg.channel.id, msg.id, "\u274c");
+
     try { 
         await bot.createMessage(msg.channel.id, await cmd.execute(bot, msg, args)); 
     } catch (e) { 
+        console.error(e);
         await bot.createMessage(msg.channel.id, `There was an error: "${e}"`); 
     }
 }
